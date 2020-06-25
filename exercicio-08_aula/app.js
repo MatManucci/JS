a84155061d4aa4ec7e35c84ba36d0664794b9a61
MatManucci <matheus.manucci@gmail.com>
2020-06-25 11:28:23 -0300

    exercicios js após formatar a maquina

---
+++ exercicio-08_aula/app.js
@@ -0,0 +1,13 @@
+/* const myFunc = function(name, lastName){
+  console.log(`Oi, ${name} ${lastName}!`)
+}
+
+myFunc('Matheus', 'Manucci') */
+
+const double = function(number){
+  return number * 2
+}
+
+const result = double(44)
+
+console.log(result)
